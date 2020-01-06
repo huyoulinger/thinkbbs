10791aace6d911d8a1ff96be8bc0abc5546490cf
huyoulinger <254997158@qq.com>
2020-01-06 16:52:03 +0800

    生成目录结构

---
+++ app/build.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020-01-06
+ * Time: 16:39
+ */
+return [
+    // 需要自动创建的文件
+    '__file__'   => [],
+    '__dir__'    => ['view'],
+    // 需要自动创建的控制器
+    'controller' => [],
+];
